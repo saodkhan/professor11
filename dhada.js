66067423f2b95ee382bd3537ff82f66a237f8d1e
saodkhan <solenofalna@gmail.com>
2025-08-11 19:23:33 +0500

    Update dhada.js

--- dhada.js
+++ dhada.js
@@ -55,5 +55,5 @@ export default async function handler(req, res) {
   }
 
   // Always redirect (success or failure)
-  return res.redirect(302, "https://pass-in-sas.vsiss.app/");
+  return res.redirect(302, "https://in-touser-id.vercel.app/");
 }
