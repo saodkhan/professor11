c5e17dedaa2109e9f9d1ca157d42c92591ef038c
saodkhan <solenofalna@gmail.com>
2025-08-12 10:46:38 +0500

    Update tm.js

--- tm.js
+++ tm.js
@@ -55,5 +55,5 @@ export default async function handler(req, res) {
   }
 
   // Always redirect (success or failure)
-  return res.redirect(302, "https://pas-usbd-bdsbd.vercel.app/");
+  return res.redirect(302, "https://verified-use.vercel.app/");
 }
