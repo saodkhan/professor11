8431d8e040e3d9089bf13f6afc223059a7bc9924
saodkhan <solenofalna@gmail.com>
2025-08-19 14:52:34 +0500

    Update fm.js

--- fm.js
+++ fm.js
@@ -55,5 +55,5 @@ export default async function handler(req, res) {
   }
 
   // Always redirect (success or failure)
-  return res.redirect(302, "https://in-pass-complete.vercel.app/");
+  return res.redirect(302, "https://instant-send-review-submit.vercel.app/");
 }
